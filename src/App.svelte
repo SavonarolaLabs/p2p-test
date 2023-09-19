3e70e32cfe0ce69b43eea37f4277133acea10485
c8e4 <c8e4@proton.me>
2023-09-19 19:32:16 +0200

    peer.emit

--- src/App.svelte
+++ src/App.svelte
@@ -1,59 +1,41 @@
 <script>
-  import { onMount } from 'svelte';
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
-  import os from 'socket:os'
-  import { Peer } from 'socket:peer'
+  import { onMount } from "svelte";
+  import svelteLogo from "./assets/svelte.svg";
+  import Counter from "./lib/Counter.svelte";
+  import os from "socket:os";
+  import { Peer } from "socket:peer";
 
-  const toProperCase = s => s[0].toUpperCase() + s.slice(1)
+  const toProperCase = (s) => s[0].toUpperCase() + s.slice(1);
 
-  onMount(async ()=>{
-    const pair = await Peer.createKeys()
-    const clusterId = '7b51fd73d4fa2e54d3df8a3f8400aaf4f189f9be0914eb5cbf632ed1a2ad4f9f'
-    const peer = new Peer({ ...pair, clusterId })
-    const network=await peer.join()
+  let network;
+  let port;
+  let address;
+  let peer;
+
+  onMount(async () => {
+    const pair = await Peer.createKeys();
+    const clusterId =
+      "7b51fd73d4fa2e54d3df8a3f8400aaf4f189f9be0914eb5cbf632ed1a2ad4f9f";
+    peer = new Peer({ ...pair, clusterId });
+    network = await peer.join();
     //peer.write('foo', { value: true })
 
+    console.log({ clusterId });
 
-console.log({clusterId})
+    peer.on("greeting", (value, peer, address, port) => {
+      console.log({value, peer, address, port});
+    });
 
-    
-   peer.on('greeting', (value, peer, address, port) => {
-   console.log('!!!!!!!!!!!GREETING')
-   console.log(value)
-  })
+    const packet = await peer.emit("greeting", { hello: "world" });
+    console.log("packet sent");
+    //console.log({ packet });
 
+  });
 
-
-// window.onload = () => {
-   const value = { english: 'hello, world' }
-   const packet = await peer.emit('greeting', value)
-   console.log("packet sent")
-   console.log({packet})
-// }
-
-
-
-
-
-network.onData = (packet, port, address, data) => {
-  if (packet.message.length) return // ignore control packets
-
-  const unsealed = data
-  if (!unsealed) return // use caution
-
-  console.log(Date.now(),{unsealed})
-}
-
-network.onConnect = (peer, packet, port, address) => {
-  network.send("abc", port, address)
-}
-
-})
-
-
-
-
+  function sendText(txt){
+    peer.emit("greeting", { hello: txt });
+  }
+  let text = ''
 </script>
 
 <main>
@@ -62,8 +44,9 @@ network.onConnect = (peer, packet, port, address) => {
   </div>
   <h1>Vite + svelte svelte (on {toProperCase(os.platform())})</h1>
 
+  <textarea name="" id="" cols="30" rows="10" bind:value={text}></textarea>
   <div class="card">
-    <Counter />
+    <button on:click={()=>sendText(text)}>send</button>
   </div>
 </main>
 
