3b7dc696557d57878fac49a39115d58da860dfb2
c8e4 <c8e4@proton.me>
2023-09-22 23:20:00 +0200

    basic p2p sample

--- src/App.svelte
+++ src/App.svelte
@@ -1,42 +1,54 @@
 <script>
   import { onMount } from "svelte";
   import svelteLogo from "./assets/svelte.svg";
-  import Counter from "./lib/Counter.svelte";
   import os from "socket:os";
-  import { Peer } from "socket:peer";
 
   const toProperCase = (s) => s[0].toUpperCase() + s.slice(1);
 
-  let network;
-  let port;
-  let address;
-  let peer;
-  let text = ''
+  let text = '';
+  let subcluster;
+
+  import { network, Encryption } from 'socket:network'
 
   onMount(async () => {
-    const pair = await Peer.createKeys();
-    const clusterId = "7b51fd73d4fa2e54d3df8a3f8400aaf4f189f9be0914eb5cbf632ed1a2ad4f9f";
-    peer = new Peer({ ...pair, clusterId });
-    network = await peer.join();
-    console.log({ clusterId });
+    console.log('onMount')
+    const sharedKey = await Encryption.createSharedKey()
+    const peerId = await Encryption.createId();
+    const clusterId = await Encryption.createClusterId();
+    console.log({sharedKey, peerId, clusterId})
 
-    peer.on("greeting", (value, peer, address, port) => {
-      console.log({value, peer, address, port});
-    });
+    const socket = await network({
+      peerId,
+      clusterId
+    })
+    console.og({socket})
 
-    const packet = await peer.emit("greeting", { hello: "world" });
-    console.log("packet sent");
+    socket.on('#ready', () => {
+      console.log('ready')
+    })
+
+    subcluster = await socket.subcluster({ sharedKey })
+    console.og({subcluster})
+    subcluster.on('message', async (value, packet) => {
+      console.log(JSON.stringify(value))
+    })
   });
 
-  function sendText(txt){
-    peer.emit("greeting", { hello: txt });
+  async function sendText(txt){
+    console.log("sendText");
+
+    //peer.emit("greeting", { hello: txt });
+    const opts = await Encryption.createKeyPair()
+    const message = { foo: txt }
+    console.log({opts});
+
+    console.log("before subcluster.emit");
+    const packets = await subcluster.emit('message', message, opts);
+    console.log("after subcluster.emit");
   }
 </script>
 
 <main>
-  <div>
-    <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-  </div>
   <h1>Vite + svelte svelte (on {toProperCase(os.platform())})</h1>
 
   <textarea name="" id="" cols="30" rows="10" bind:value={text}></textarea>
@@ -44,17 +56,3 @@
     <button on:click={()=>sendText(text)}>send</button>
   </div>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-</style>
